9fdd03899645477c0d25e7e9cacaf00cfc405c5b
Pedro Otávio <pedrootavio09@yahoo.com.br>
2024-08-01 22:24:15 -0300

    Final Project

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-function tocaSom(idElement){
-    document.querySelector(idElement).play();
+function tocaSom(seletorAudio){
+    const elemento = document.querySelector(seletorAudio);
+
+    if(elemento && elemento.localName ==="audio"){
+        elemento.play();
+    }
+    else {
+        alert("Element not found.");
+        console.log("Element not found.");
+    }
 }
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
@@ -10,5 +18,16 @@ for(let i= 0; i < listaDeTeclas.length; i++) {
 
     listaDeTeclas[i].onclick = function() {
         tocaSom(`#som_${instrumento}`);
+    }
+
+    tecla.onkeydown = function (event) {
+        if(event.code === "Enter" || event.code === "Space"){
+            tecla.classList.add('ativa');
+        }
+    }
+    tecla.onkeyup = function (event) {
+        if(event.code === "Enter" || event.code === "Space") {
+            tecla.classList.remove('ativa');
+        }
     }
 }
